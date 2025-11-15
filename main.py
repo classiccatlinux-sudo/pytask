f6d64e89257a27b61950fc59331a9453cff0af59
classic14 <classiccatlinux@gmail.com>
2025-11-15 16:17:21 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import time
+
+#main
+print("welcome to pytask a simple program manager for linux systems")
+time.sleep(1)
+print("powered by htop")
+time.sleep(1)
+os.system("clear")
+os.system("sudo apt install htop -y")
+os.system("clear")
+print("htop has been installed successfully")
+time.sleep(1)
+print("how to use htop:")
+print("1. Press F10 to exit")
+print("2. Use arrow keys to navigate")
+print("3. Press F3 to search for a process")
+print("4. Press F9 to kill a process")
+time.sleep(8)
+os.system("clear")
+os.system("htop")
+time.sleep(1)
+os.system('clear')
+
+
+
+
+
+
+
+
+
+
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install python3 -y
+echo python3 installed
+clear
+python3 main.py
